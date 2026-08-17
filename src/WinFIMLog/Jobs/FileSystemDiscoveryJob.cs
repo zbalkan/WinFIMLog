812a346e8016c3b71ae1dbdba46f9d38955c62b2
Zafer Balkan <zafer@zaferbalkan.com>
2026-08-17 10:01:18 +0300

    Removed old license notice text

--- src/WinFIMLog/Jobs/FileSystemDiscoveryJob.cs
+++ src/WinFIMLog/Jobs/FileSystemDiscoveryJob.cs
@@ -10,14 +10,6 @@ using WinFIMLog.Data;
 using WinFIMLog.FIM;
 using WinFIMLog.IO;
 
-// {{ FIM }} Copyright (C) {{ 2022 }} {{ Zafer Balkan }}
-//
-// This program is free software: you can redistribute it and/or modify it under the terms of the
-// GNU Affero General Public License as published by the Free Software Foundation, either version 3
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY
-
 namespace WinFIMLog.Jobs
 {
     internal class FileSystemDiscoveryJob
